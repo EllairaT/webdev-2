3ebc727f4ad86aa8347c91709ebf1d6b39e58278
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-05-25 03:56:34 +1200

    updates

--- bookingprocess.php
+++ bookingprocess.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 header('Content-Type: application/json');
 
@@ -9,8 +10,8 @@ function validateSuburb($param)
 {
     global $conn;
 
-    $search_query = $conn->prepare("SELECT `SUBURB_NAME` FROM `suburbs` WHERE  `SUBURB_NAME` = ?");
-    $search_query->bind_param("s", $searchvar);
+    $search_query = $conn->prepare('SELECT `SUBURB_NAME` FROM `suburbs` WHERE  `SUBURB_NAME` = ?');
+    $search_query->bind_param('s', $searchvar);
 
     $searchvar = $param;
 
@@ -20,55 +21,169 @@ function validateSuburb($param)
     //check if db returns a result
     if ($search_query->num_rows() > 0) {
         return true;
-    } else {
-        return false;
     }
+
+    return false;
 }
 
-$response = array('status' => 0, 'details' => '');
+function insertCustomer($arr)
+{
+    global $conn;
+    $sql_customer = 'INSERT INTO `customers`(`CUSTOMER_NAME`, `CUSTOMER_PH`) VALUES (?,?)';
+    $insert_customer = $conn->prepare($sql_customer);
+    $insert_customer->bind_param('ss', $cname, $cphone);
 
-$search_query =  $conn->prepare("SELECT `SUBURB_NAME` FROM `suburbs` WHERE  `SUBURB_NAME` LIKE ? ");
-$search_query->bind_param("s", $searchvar);
+    $cname = $arr['cname'];
+    $cphone = $arr['phone'];
 
-$searchvar;
-$suburb_arr = array();
+    if ($insert_customer->execute()) {
+        $insert_customer->store_result();
+
+        return true;
+    }
+
+    return false;
+}
+
+function insertSuburb($type, $suburb, $ref)
+{
+    global $conn;
+    $column = 'origin' == $type ? 'ORIGIN_SUBURB_ID' : 'DESTINATION_SUBURB_ID';
+
+    $query = 'INSERT INTO `booking_information`( ? )
+                SELECT `SUBURB_ID`
+                FROM `suburbs`
+                WHERE `REF_ID` = ? 
+                AND `suburbs`.`SUBURB_NAME` = ?';
+
+    $insert = $conn->prepare($query);
+    $insert->bind_param('sss', $column, $ref, $suburb);
+
+    if ($insert->execute()) {
+        $insert->store_result();
+
+        return true;
+    }
+
+    return false;
+}
+
+function insertInfo($arr)
+{
+    global $conn;
+    $param_arr = [];
+
+    $sql_info = 'INSERT INTO `booking_information`(`REF_NUM`, `UNIT_NUMBER`, `STREET_NUMBER`, `STREET_NAME`, 
+                               `PICKUP_DATE`, `PICKUP_TIME`) 
+                 VALUES (?,?,?,?,?,?,?,?)';
+
+    $insert_info = $conn->prepare($sql_info);
+    $insert_info->bind_param('ssssss', $searchvar);
+
+    //if isset origin and destination subs
+    foreach ($arr as $a) {
+    }
+
+    if (!$insert_info->execute()) {
+        return false;
+    }
+    $insert_info->store_result();
+}
+
+function insertBooking($arr)
+{
+    global $conn;
+    //two statements, but one transaction.
+    $sql_booking = 'INSERT INTO `bookings`(`CUSTOMER_ID`)
+    SELECT CUSTOMER_ID 
+    FROM customers
+    WHERE customers.CUSTOMER_NAME = ?
+    AND customers.CUSTOMER_PH = ?;
+    
+    INSERT INTO `bookings`(`CUSTOMER_ID`, `BOOKING_REF`, `BOOKING_DATE`) 
+    VALUES (?,?,?)';
+
+    $insert_booking = $conn->prepare($sql_booking);
+    $insert_booking->bind_param('ssssss', $searchvar);
+
+    if (!$insert_booking->execute()) {
+        return false;
+    }
+    $insert_booking->store_result()
+    ;
+}
+
+// executes all the queries
+function submit($arr)
+{
+    global $conn;
+    if (insertCustomer($arr) && insertInfo($arr) && insertBooking($arr)) {
+        return true;
+    }
+
+    return false;
+}
+
+// creates a reference number for user
+function createRef()
+{
+    $time = time();
+    $bytes = random_bytes(8);
+    $hex = bin2hex($bytes);
+    $ref = strtoupper(substr($time, -4).$hex);
+
+    return implode('-', str_split($ref, 4));
+}
+
+$response = ['status' => 0, 'details' => '', 'data' => ''];
+
+$search_query = $conn->prepare('SELECT `SUBURB_NAME` FROM `suburbs` WHERE  `SUBURB_NAME` LIKE ? ');
+$search_query->bind_param('s', $searchvar);
+
+$suburb_arr = [];
+
+$form_arr = [];
 
 if (isset($_GET['sbname'])) {
-    $searchvar = '%' . $_GET['sbname'] . '%';
+    $searchvar = '%'.$_GET['sbname'].'%';
 
     $search_query->execute();
 
     $search_query->store_result();
     $search_query->bind_result($suburb);
 
-
     if ($search_query->num_rows() > 0) {
         while ($search_query->fetch()) {
             array_push($suburb_arr, $suburb);
         }
     }
-
     $response['data'] = $suburb_arr;
 } elseif (isset($_GET['dsname'])) {
-    $searchvar = '%' . $_GET['dsname'] . '%';
+    $searchvar = '%'.$_GET['dsname'].'%';
 
     $search_query->execute();
 
     $search_query->store_result();
     $search_query->bind_result($suburb);
 
-
     if ($search_query->num_rows() > 0) {
         while ($search_query->fetch()) {
             array_push($suburb_arr, $suburb);
         }
     }
-
     $response['data'] = $suburb_arr;
 } elseif (isset($_GET['validateSub'])) {
     if (validateSuburb($_GET['validateSub'])) {
         $response['status'] = 'Success';
     }
+} elseif (isset($_POST['submitted'])) {
+    foreach ($_POST as $k) {
+        array_push($form_arr, $k);
+    }
+    //submit($_POST);
+    $form_arr['customerref'] = createRef();
+    $response = $form_arr;
 }
 
 echo json_encode($response);
+//echo insertSuburb('des', '', '');
--- js/booking.js
+++ js/booking.js
@@ -1,66 +1,88 @@
 //do this on page load
+var date = moment();
 $(function () {
-  var date = new Date(),
-    h = date.getHours(),
-    m = date.getMinutes();
-
   //set current date
-  //max date is 2 months from the current date.
-  //this is so that no one can book a year in advance.
-  $("#dateID").datepicker({
-    dateFormat: "dd/mm/yy",
-    minDate: date,
-    maxDate: "+2m",
-  });
-  $("#dateID").datepicker("setDate", date);
 
-  $("#timeID").timepicker({
-    timeFormat: "HH:mm",
-    interval: 15,
-    maxTime: date,
-    defaultTime: "now",
-    dynamic: true,
-    dropdown: true,
-    scrollbar: false,
-    change: function () {
-      validateTime();
-    },
+  //initialise date and time ------------
+  $("#timeID").val(date.format("HH:mm"));
+  $("#dateID").val(date.format("D/MM/YYYY"));
+
+  $("#timeID").bootstrapMaterialDatePicker({
+    date: false,
+    format: "HH:mm",
+    nowButton: true,
+    okText: "Next",
+    minDate: date,
   });
+
+  $("#dateID").bootstrapMaterialDatePicker({
+    weekStart: 0,
+    time: false,
+    minDate: date,
+    format: "DD/MM/YYYY",
+  });
+
+  $("#dateID").on("change", function () {
+    dateWasChanged();
+  });
+  // -------------------------------------
 
   //update time to NOW when now btn is pressed
   $("#btnNowID").on("click", function (e) {
+    $("#timeID").val(date.format("HH:mm"));
     e.preventDefault();
-    $("#timeID").timepicker("setTime", new Date());
+  });
+
+  $("#btnTodayID").on("click", function (e) {
+    $("#dateID").val(date.format("D/MM/YYYY"));
+    dateWasChanged();
+    e.preventDefault();
   });
 
   // suggest suburb name
-  $("#sb-nameID").on("keyup", function () {
+  $("#sb-nameID").on("keydown", function () {
     getSuburbData($("#sb-nameID"));
   });
 
   // suggest destination suburb
-  $("#ds-nameID").on("keyup", function () {
+  $("#ds-nameID").on("keydown", function () {
     getSuburbData($("#ds-nameID"));
   });
 
-  //validate input once user focuses out of input
-  $("#sb-nameID").on("focusout", function () {
-    validateSuburb($("#sb-nameID"));
+  //validate inputs once user focuses out of input
+  $("#sb-nameID").on("change", function () {
+    if (!($("#sb-nameID").val() == "")) {
+      validateSuburb($("#sb-nameID"));
+    } else {
+      console.log("null. not validating.");
+    }
   });
 
-  $("#ds-nameID").on("focusout", function () {
-    validateSuburb($("#ds-nameID"));
+  $("#ds-nameID").on("change", function () {
+    if (!($("#ds-nameID").val() == "")) {
+      validateSuburb($("#ds-nameID"));
+    } else {
+      console.log("null. not validating.");
+    }
   });
 
   $("#phoneID").on("change", function () {
-    validatePhone($("#phoneID"));
+    validatePhone();
   });
 
-  //validate form before submitting
-  $("#submitID").on("click", function (e) {
-    e.preventDefault();
+  $("#timeID").on("change", function () {
+    validateTime();
+  });
 
+  $("#dateID").on("change", function () {
+    validateTime();
+  });
+
+  $("#submitID").on("click", function (e) {
+    //validate form before submitting
+    e.preventDefault();
     if (validateForm()) {
+      sendFormData();
       console.log("submitted!");
     }
   });
@@ -72,6 +94,7 @@ var url = "bookingprocess.php";
 //return all suburb names
 function getSuburbData(val) {
   xhr.open("GET", url + "?" + val.attr("name") + "=" + val.val());
+  console.log(url + "?" + val.attr("name") + "=" + val.val());
   xhr.responseType = "json";
   xhr.onload = function () {
     if (this.status == 200) {
@@ -111,7 +134,9 @@ function validateForm() {
     .find("input") //find all inputs
     .each(function () {
       //get required fields
+
       if ($(this).prop("required")) {
+        console.log("checking: " + $(this).attr("name"));
         // if field is empty, add a false flag to array
         if (!$(this).val()) {
           $(this).addClass("is-invalid");
@@ -120,14 +145,17 @@ function validateForm() {
           $(this).removeClass("is-invalid");
           flags.push(true);
         }
+        console.log($(flags).get(-1));
       }
     });
 
-  if (validateTime() && validatePhone()) {
+  if (validatePhone()) {
     flags.push(true);
   } else {
     flags.push(false);
   }
+
+  console.log(flags);
   if (flags.includes(false)) {
     return false;
   } else {
@@ -137,10 +165,20 @@ function validateForm() {
 
 // send entire form as json to server
 function sendFormData() {
-  var dataToSend = $("#booking-form").serialize();
-
+  var dataToSend = new FormData(document.getElementById("booking-form"));
+  //extra parameter to let the server know that the form has been submitted.
+  dataToSend.append("submitted", true);
   xhr.open("POST", url);
-  xhr.onreadystatechange = function () {};
+  xhr.responseType = "json";
+  xhr.onload = function () {
+    if (this.status == 200) {
+      console.log(this.response);
+    } else {
+      console.log("Bad request");
+    }
+  };
+
+  xhr.send(dataToSend);
 }
 
 function showValidation(val, bool) {
@@ -160,30 +198,42 @@ function showValidation(val, bool) {
   }
 }
 
+//if selected date is today, validate time based on time left in the day
 function validateTime() {
-  var date = new Date();
-  var selectedTime = $("#timeID").val();
-  var currentTime = date.toLocaleTimeString([], { hour12: false }).slice(0, -3);
-  var regexp = new RegExp("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$");
-  if (regexp.test(selectedTime)) {
-    if (selectedTime >= currentTime) {
-      $("#timeID").addClass("is-valid");
-      $("#timeID").removeClass("is-invalid");
-      return true;
-    } else {
-      $("#timeID").addClass("is-invalid");
-      $("#timeID").removeClass("is-valid");
-    }
+  var selectedTime = moment($("#timeID").val(), "HH:mm", true);
+  if (selectedTime.isValid()) {
+    $("#timeID").val(selectedTime.format("HH:mm"));
+    showValidation($("#timeID"), true);
   } else {
-    $("#timeID").addClass("is-invalid");
-    $("#timeID").removeClass("is-valid");
+    showValidation($("#timeID"), false);
   }
   return false;
 }
 
+function dateWasChanged() {
+  var currentDate = moment(date, "DD/MM/YYYY", true);
+  var currentTime = moment(date, "HH:mm", true);
+  var selectedDate = moment($("#dateID").val(), "DD/MM/YYYY", true);
+  var selectedTime = moment($("#timeID").val(), "HH:mm", true);
+
+  var nextDay = moment(date).add(1, "days");
+  console.log(nextDay);
+
+  // reset the time picker if the selected date is after current date
+  if (selectedDate.isAfter(currentDate)) {
+    $("#timeID").val("00:00");
+    $("#timeID").bootstrapMaterialDatePicker("setMinDate", selectedDate);
+  } else {
+    console.log(selectedDate, currentDate);
+    $("#timeID").val(currentTime.format("HH:mm"));
+    $("#timeID").bootstrapMaterialDatePicker("setMinDate", currentDate);
+  }
+  return true;
+}
+
 function validatePhone() {
   var val = $("#phoneID");
-  var regexp = new RegExp("^(+0?1s)?(?d{3})?[s.-]d{3}[s.-]d{4}$");
+  var regexp = new RegExp("^[ 0-9]{10,12}$");
   if (regexp.test(val.val())) {
     showValidation(val, true);
     return true;
